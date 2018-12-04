15ae6f669b19157c92eff7516ab12b4b1bd23e00
yus72 <yus72@pitt.edu>
2018-12-03 22:12:51 -0500

    图片验证码的前端实现

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.views import APIView
-from meiduo_mall.meiduo_mall.libs.captcha.captcha import captcha
+from meiduo_mall.libs.captcha.captcha import captcha
 from django_redis import get_redis_connection
 
 from . import constants
