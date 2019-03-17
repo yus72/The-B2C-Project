975c5e3d03703c8735208769e383e0eae244d334
yus72 <yus72@pitt.edu>
2019-03-17 16:39:17 -0400

    Using the django-crontab to achieve timed task

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'areas.apps.AreasConfig',
     'contents.apps.ContentsConfig',
     'goods.apps.GoodsConfig',
+    'django_crontab',  # 定时任务
 ]
 
 MIDDLEWARE = [
@@ -301,3 +302,14 @@ CKEDITOR_UPLOAD_PATH = ''  # 上传图片保存路径，使用了FastDFS，所�
 
 # 页面静态化保存文件的目录
 GENERATED_STATIC_HTML_FILES_DIR = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'front_end_pc')
+
+
+# 定时任务
+CRONJOBS = [
+    # 每5分钟执行一次生成主页静态文件
+    ('*/5 * * * *', 'contents.crons.generate_static_index_html', '>> /home/python/Desktop/django_project/The-B2C-Project/meiduo_mall/logs/crontab.log')
+    # ('*/1 * * * *', 'contents.crons.generate_static_index_html', '>> '+ os.path.join(os.path.dirname(BASE_DIR), "/logs/crontab.log"))
+]
+
+
+CRONTAB_COMMAND_PREFIX = 'LANG_ALL=zh_cn.UTF-8'
