60529716092d0d5e395e42df3549d0ccf51cbb95
yus72 <yus72@pitt.edu>
2019-03-19 18:52:37 -0400

    The front-end and back-end realizations of searching page

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListAPIView
 from goods.models import SKU
-from .serializers import SKUSerializer
-
+from .serializers import SKUSerializer,SKUIndexSerializer
+from drf_haystack.viewsets import HaystackViewSet
 
 # GET /categories/(?P<category_id>\d+)/skus?page=xxx&page_size=xxx&ordering=xxx
 class SKUListView(ListAPIView):
@@ -19,3 +19,11 @@ class SKUListView(ListAPIView):
     def get_queryset(self):
         category_id = self.kwargs['category_id']
         return SKU.objects.filter(category_id=category_id, is_launched=True)
+
+class SKUSearchViewSet(HaystackViewSet):
+    """
+    SKU搜索
+    """
+    index_models = [SKU]
+
+    serializer_class = SKUIndexSerializer
--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'contents.apps.ContentsConfig',
     'goods.apps.GoodsConfig',
     'django_crontab',  # 定时任务
+    'haystack',  # 对接搜索引擎
 ]
 
 MIDDLEWARE = [
@@ -323,3 +324,18 @@ CRONJOBS = [
 
 
 CRONTAB_COMMAND_PREFIX = 'LANG_ALL=zh_cn.UTF-8'
+
+
+
+# Haystack
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://192.168.243.129:9200/',  # 此处为elasticsearch运行的服务器ip地址，端口号固定为9200
+        'INDEX_NAME': 'meiduo',  # 指定elasticsearch建立的索引库的名称
+    },
+}
+
+# 当添加、修改、删除数据时，自动生成索引
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+
